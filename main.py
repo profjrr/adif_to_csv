cf418642422875c765437100473c2c0c26f8865c
tim <tim@sy-edm.com>
2016-06-21 07:34:46 +0400

    Fixed Variable format processing, plus changed the init method.

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ from adif2csv import adif2csv
 import sys
 
 if len(sys.argv)!=2:
-    print("Error: Expected main.py <ADIF>")
+    print("Error: Expected python main.py <ADIF>")
     exit()
 else:
-    cvt=adif2csv()
+    cvt=adif2csv(False)
     cvt.process(sys.argv[1])
     for a in cvt.dump():
         print(''+a, end='')
